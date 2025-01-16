92714f5863726497b6508f240fc8f7316612945e
phunatcha.k <phunatcha.k@gmail.com>
2025-01-16 21:33:32 +0700

    edit(ScoreAnn):แก้ validate form search

--- src/app/components/score-announcement/search-form/search-form-search-form-score-announcemen.component.ts
+++ src/app/components/score-announcement/search-form/search-form-search-form-score-announcemen.component.ts
@@ -92,10 +92,10 @@ export class SearchFormScoreAnnouncementComponent implements OnInit {
     this.form = this.fb.group({
       subjectSearch: ['', Validators.required],
       studentSearch: [{ value: '' }],
-      semester: [{ value: null }, Validators.required],
-      section: [{ value: null }, Validators.required],
+      semester: [ null , Validators.required],
+      section: [ null , Validators.required],
       sendStatus: [{ value: null }],
-      academic_year: [{ value: null }, Validators.required],
+      academic_year: [ null , Validators.required],
     });
 
     this.contantLovService
