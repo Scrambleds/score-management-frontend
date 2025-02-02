cae35c182639ddbe92d0461c98fa3aed2969edba
firstfear1412 <78691900+firstfear1412@users.noreply.github.com>
2025-02-03 01:08:33 +0700

    add ingore list spinner loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ import { LoadingSpinnerComponent } from './components/loading-spinner/loading-sp
 export class AppComponent implements OnInit {
   public spinkit = Spinkit;
   public customSpinner = LoadingSpinnerComponent;
+  ignoredUrls: string[] = ['/api/MasterData/Subject'];
 
   searchCriteria: any = {};
   isOpen: boolean = false;
